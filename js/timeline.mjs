d42978b7032a2e04f88c8c80490396c01886ac8d
yashrajbharticybtekk <y.bharti@cybtekk.com>
2023-06-03 16:39:25 +0530

    ADDED ALL PAGES

--- js/timeline.mjs
+++ js/timeline.mjs
@@ -1,26 +1,2 @@
-gsap.registerPlugin(ScrollTrigger);
-
-const tl = gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: ".circle",
-      scrub: true,
-      pin: true,
-      start: "0% 0%",
-      end: "+=0%",
-      duration: 5,
-    },
-  })
-
-  .from(".circle", {
-    scale: 1,
-    ease: "ease",
-    duration: 1,
-  })
-  .to(".circle", {
-    scale: 10,
-    ease: "ease",
-    duration: 1,
-  });
-tl.reverse();
+const tl = {};
 export { tl };
