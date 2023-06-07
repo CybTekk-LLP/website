b768d8936434f1819262d7132ab2508abd6482f3
yashrajbharticybtekk <y.bharti@cybtekk.com>
2023-06-07 14:12:41 +0530

    added masonry layout

---
+++ js/masonry.js
@@ -0,0 +1,19 @@
+var colWidth = $(".card").width();
+
+window.onresize = function () {
+  var colWidth = $("card").width();
+};
+console.log(colWidth);
+
+var $grid = $(".container").masonry({
+  // options
+  itemSelector: ".card",
+  columnWidth: ".card",
+  // percentPosition: true,
+  gutter: 20,
+  fitWidth: true,
+});
+
+$grid.imagesLoaded().progress(function () {
+  $grid.masonry("layout");
+});
