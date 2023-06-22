1d59be109e97ae43b1e03ee2a66e09bcb1757c60
yashrajbharticybtekk <y.bharti@cybtekk.com>
2023-06-22 16:01:50 +0530

    added meta tags

--- js/underline.js
+++ js/underline.js
@@ -4,17 +4,6 @@ let underline = document.querySelector(".underline");
 let underlinedesign = function () {
   underline.classList.add("animate-underline");
 };
-// window.addEventListener(
-//   "scroll",
-//   function (event) {
-//     if (isInViewport(element)) {
-//       design();
-//     } else {
-//       undesign();
-//     }
-//   },
-//   false
-// );
 let underlinelenis = new Lenis();
 
 underlinelenis.on("scroll", (e) => {
