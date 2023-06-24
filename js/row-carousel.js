c81a56ad75c86a8b1b9f99f7c2b919fe465512a3
yashrajbharticybtekk <y.bharti@cybtekk.com>
2023-06-24 15:09:48 +0530

    added band svg

--- js/row-carousel.js
+++ js/row-carousel.js
@@ -1,15 +1,11 @@
-let iconContainer = document.querySelector(".keep-inline .big-img");
-let x = 0;
-setInterval(() => {
-  iconContainer.style.transform = "translateX(+" + x + "px)";
-  iconContainer.style.transition =
-    "transform 0.6s cubic-bezier(.215,.61,.355,1)";
-  x -= 168;
-  let node = iconContainer.lastChild;
-  iconContainer.removeChild(iconContainer.lastChild);
-  iconContainer.prepend(node);
-}, 1000);
-
-// for(let icons of iconTranslate){
-//     let prev = icons.src
-//  }
+// let iconContainer = document.querySelector(".keep-inline .big-img");
+// let icons = document.querySelectorAll(".keep-inline .big-img img");
+// let x = 0;
+// setInterval(() => {
+//   iconContainer.style.transition = "transform 0.6s ease";
+//   iconContainer.style.transform = "translateX(-" + x + "px)";
+//   x = 168 + x;
+//   let node = iconContainer.lastChild;
+//   iconContainer.removeChild(iconContainer.lastChild);
+//   iconContainer.prepend(node);
+// }, 1200);
