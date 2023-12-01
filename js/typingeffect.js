0e872b1032c85987b699fbd820fc8c2497623c4f
yashrajbharticybtekk <y.bharti@cybtekk.com>
2023-12-01 11:48:24 +0530

    code changes

--- js/typingeffect.js
+++ js/typingeffect.js
@@ -3,6 +3,8 @@ $("#typed").typed({
     "We are an App Development Agency.",
     "We are a Web Development Agency.",
     "We are a Solution Agency.",
+    "We are a Web Design Agency.",
+    "We are a Digital Marketing Agency.",
   ],
   typeSpeed: 100,
   startDelay: 0,
